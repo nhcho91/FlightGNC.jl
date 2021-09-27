4ba5236775b4b6c770a56ecc98ad36c979dd40f4
Namhoon Cho <nhcho91@gmail.com>
2021-09-27 12:02:51 +0100

    housekeeping shell script

--- src/environments/missiles.jl
+++ src/environments/missiles.jl
@@ -50,7 +50,7 @@ function Dynamics!(env::PursuerEvadorMissile)
     @unpack pursuer, evador = env
     @Loggable function dynamics!(dx, x, params, t; u_pursuer, u_evador)
         @onlylog r   = norm(x.pursuer.p - x.evador.p)
-        @onlylog σ_M = atan(norm(cross(x.evador.p - x.pursuer.p, x.pursuer.v)), dot(x.evador.p - x.pursuer.p, x.pursuer.v))
+        @onlylog σ_M = acos( clamp( dot( normalize(x.evador.p - x.pursuer.p), normalize(x.pursuer.v) ), -1, 1 ) )
         
         @nested_log :pursuer Dynamics!(pursuer)(dx.pursuer, x.pursuer, nothing, t; u = u_pursuer)
         @nested_log :evador Dynamics!(evador)(dx.evador, x.evador, nothing, t; u = u_evador)
